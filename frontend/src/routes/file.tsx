10f2ebae92e298fddba038859796dc23d019702b
Rashik Shahjahan <rashikshahjahan@protonmail.com>
2024-10-29 13:01:24 -0400

    added my files list and dowload

--- frontend/src/routes/file.tsx
+++ frontend/src/routes/file.tsx
@@ -1,4 +1,39 @@
+import axios from 'axios'
+import { useEffect, useState } from 'react'
+
 export default function File({ owner }: { owner: boolean }) {
-  return <div>{owner ? 'My Files' : 'Shared Files'}</div>
+  const [files, setFiles] = useState<File[]>([])
+
+  useEffect(() => {
+    const getFiles = async () => {
+      const res = await axios.get(import.meta.env.VITE_FILES_URL)
+      setFiles(res.data.Contents)
+    }
+    getFiles()
+  }, [])
+  
+  const downloadFile = async (key: string) => {
+      const res = await axios.get(import.meta.env.VITE_FILES_URL + '/' + key, {
+      responseType: 'blob',
+    })
+    const url = window.URL.createObjectURL(new Blob([res.data]))
+    const a = document.createElement('a')
+    a.href = url
+    a.download = key
+    a.click()
+  }
+  return(
+    <div>
+      {files.map((file) => (
+          <div key={file.Key}>
+          {file.Key}
+          {file.LastModified}
+          {file.Size}
+          <button onClick={() => downloadFile(file.Key)}>Download</button>
+        </div>
+       ))}
+    </div>
+  )
+    
 }
 
